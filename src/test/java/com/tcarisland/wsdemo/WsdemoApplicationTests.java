b8402be2a1685f724d752c6c2f49dd4cbb3f3860
Thor Arisland <tcarisland@gmail.com>
2023-06-01 22:42:48 +0200

    project

---
+++ src/test/java/com/tcarisland/wsdemo/WsdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tcarisland.wsdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
